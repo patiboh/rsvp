0fcb1b7b9d6f89bdb44caa126fafc17dd8abfddc
patiboh <lapatiboh@gmail.com>
2020-09-23 12:34:40 +0200

    ➕ Add Svelte package

---
+++ packages/svelte/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import HelloWorld from './components/HelloWorld.svelte'
+</script>
+
+<style>
+  .app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
+
+<main class="app">
+  <HelloWorld name="World" />
+</main>
---
+++ packages/svelte/src/components/HelloWorld.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let name
+</script>
+
+<style>
+  .greeting {
+    color: fuchsia;
+  }
+</style>
+
+<h1 class="greeting">Hello {name}</h1>
