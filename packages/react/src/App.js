978fdb8788f8dbdea68eff6872fd3940140b659b
patiboh <lapatiboh@gmail.com>
2020-09-23 13:29:16 +0200

    💄 Style React

--- packages/react/src/App.js
+++ packages/react/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import HelloWorldJSX from './components/HelloWorldJSX';
 // import HelloWorldCreateElem from './components/HelloWorldCreateElem';
-import './App.css';
 
 function App() {
   return <HelloWorldJSX name='World'/>;
